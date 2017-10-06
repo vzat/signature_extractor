b48cf058f8d2d5298418b5ef32ef60e8448d1329
Vlad Zat <vlad.zat@mydit.ie>
2017-10-06 11:02:07 +0100

    Scalling and blurring the image

--- sinatureExtractor.py
+++ sinatureExtractor.py
@@ -31,10 +31,16 @@ signature = cv2.imread(imgsPath + "Boss.bmp")
 
 def adaptiveThreshold(img):
     g = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # TODO remove the constrants from here:
     img = cv2.adaptiveThreshold(g, maxValue = 255, adaptiveMethod = cv2.ADAPTIVE_THRESH_MEAN_C, thresholdType = cv2.THRESH_BINARY, blockSize = 5, C = 15)
     return img
 
 mask = adaptiveThreshold(signature)
+# k = np.array([[1,4,1], [4,7,4], [1,4,1]], dtype=float)
+# blurredSignature = cv2.filter2D(mask,-1,k)
+# cv2.imshow('Signature', blurredSignature)
+# key = cv2.waitKey(0)
 
 # TODO Clean the mask
 
@@ -42,6 +48,14 @@ rmask = cv2.bitwise_not(mask)
 signature = cv2.bitwise_and(signature, signature, mask=rmask)
 
 # TODO ??? Click on an image to append the signature there
+
+# Reference - http://docs.opencv.org/3.3.0/d4/d13/tutorial_py_filtering.html
+# k = np.array([[1,4,1], [4,7,4], [1,4,1]], dtype=float)
+# blurredSignature = cv2.filter2D(signature,-1,k)
+# imgs = np.hstack((signature, blurredSignature))
+
+sHeight, sWidth, _ = signature.shape
+scaledSignature = cv2.resize(signature, (2 * sWidth, 2 * sHeight))
 
 cv2.imshow('Signature', signature)
 key = cv2.waitKey(0)
