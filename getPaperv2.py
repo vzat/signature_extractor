882d71848da077f9f8c6b4cfb677d51752283c7c
Vlad Zat <vlad.zat@mydit.ie>
2017-10-09 16:48:12 +0100

    Fixed getting the signature from page but broken if the image contains only the page

---
+++ getPaperv2.py
@@ -0,0 +1,5 @@
+import numpy as np
+import cv2
+from matplotlib import pyplot as plt
+from matplotlib import image as image
+import easygui
--- signatureExtractor.py
+++ signatureExtractor.py
@@ -43,14 +43,18 @@ class Rect:
         self.y = y
         self.w = w
         self.h = h
+        self.area = 0
 
+    def setArea(self, area):
+        self.area = area
     def getArea(self):
-        return self.w * self.h
+        return self.area
     def set(self, x, y, w, h):
         self.x = x
         self.y = y
         self.w = w
         self.h = h
+        self.area = w * h
     def addPadding(self, imgSize, padding):
         self.x -= padding
         self.y -= padding
@@ -69,7 +73,7 @@ class Rect:
 
 # TODO Use easygui to open images
 imgsPath = 'images/'
-signature = cv2.imread(imgsPath + 'Boss.bmp')
+signature = cv2.imread(imgsPath + 'Trump.jpg')
 # signature = cv2.medianBlur(signature, 3)
 # signature = cv2.GaussianBlur(signature, (3, 3), 0)
 
@@ -77,7 +81,8 @@ signature = cv2.imread(imgsPath + 'Boss.bmp')
 
 def getPageFromImage(img):
     gImg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    bImg = cv2.medianBlur(src = gImg, ksize = 11)
+    # bImg = cv2.medianBlur(src = gImg, ksize = 11)
+    bImg = gImg.copy()
 
     threshold, _ = cv2.threshold(src = bImg, thresh = 0, maxval = 255, type = cv2.THRESH_BINARY | cv2.THRESH_OTSU)
     cannyImg = cv2.Canny(image = bImg, threshold1 = 0.5 * threshold, threshold2 = threshold)
@@ -89,10 +94,18 @@ def getPageFromImage(img):
 
     maxRect = Rect(0, 0, 0, 0)
     for contour in contours:
+        # Detect edges
+        # Reference - http://docs.opencv.org/3.1.0/dd/d49/tutorial_py_contour_features.html
+        epsilon = cv2.arcLength(contour, True)
+        corners = cv2.approxPolyDP(contour, 0.1 * epsilon, True)
         x, y, w, h = cv2.boundingRect(points = contour)
         currentArea = w * h
-        if currentArea > maxRect.getArea():
+        # currentArea = cv2.contourArea(contour)
+
+        # check if length of approx is 4
+        if len(corners) == 4 and currentArea > maxRect.getArea():
             maxRect.set(x, y, w, h)
+            # maxRect.setArea(currentArea)
 
     return img[maxRect.y : maxRect.y + maxRect.h, maxRect.x : maxRect.x + maxRect.w]
 
@@ -115,10 +128,16 @@ def getSignatureFromPage(img):
     _, contours, _ = cv2.findContours(image = cannyImg.copy(), mode = cv2.RETR_TREE, method = cv2.CHAIN_APPROX_SIMPLE)
 
     maxRect = Rect(0, 0, 0, 0)
+    maxCorners = 0
     for contour in contours:
+        epsilon = cv2.arcLength(contour, True)
+        corners = cv2.approxPolyDP(contour, 0.01 * epsilon, True)
         x, y, w, h = cv2.boundingRect(points = contour)
         currentArea = w * h
-        if currentArea > maxRect.getArea():
+        # Maybe add w > h ?
+        # if currentArea > maxRect.getArea():
+        if len(corners) > maxCorners:
+            maxCorners = len(corners)
             maxRect.set(x, y, w, h)
 
     # Increase the bounding box to get a better view of the signature
@@ -157,6 +176,8 @@ def getSignature(img):
     return cv2.bitwise_and(signature, signature, mask=rmask)
 
 signature = getPageFromImage(img = signature)
+cv2.imshow('Signature', signature)
+cv2.waitKey(0)
 signature = getSignatureFromPage(img = signature)
 signature = getSignature(img = signature)
 
