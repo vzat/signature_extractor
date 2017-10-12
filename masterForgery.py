8d3ac84b42f5b30f06f0c520ae626b5c076fd194
Vlad Zat <vlad.zat@mydit.ie>
2017-10-12 11:25:28 +0100

    Scale Down Big Images

--- masterForgery.py
+++ masterForgery.py
@@ -27,6 +27,33 @@ def writeImageToFile(img, mask, fileName = 'signature'):
     imgWithAlpha = cv2.merge((b, g, r, mask))
     cv2.imwrite(fileName + '.png', imgWithAlpha)
 
+def displayImageToScreen(img, mask):
+    imgSize = np.shape(img)
+    bg = np.zeros((imgSize[0], imgSize[1], 3), np.uint8)
+    bg[:, :] = (255, 255, 255)
+
+    rmask = cv2.bitwise_not(mask)
+    bgROI = cv2.bitwise_and(bg, bg, mask = rmask)
+    sigROI = cv2.bitwise_and(signature, signature, mask = mask)
+
+    roi = cv2.bitwise_or(bgROI, sigROI)
+
+    cv2.imshow('Signature', roi)
+    cv2.waitKey(0)
+
+def scaleImageDown(img):
+    imgSize = np.shape(img)
+    maxSize = (1920, 1080)
+    if imgSize[0] > maxSize[0] or imgSize[1] > maxSize[1]:
+        wRatio = float(float(maxSize[0]) / float(imgSize[0]))
+        hRatio = float(float(maxSize[1]) / float(imgSize[1]))
+        ratio = 1.0
+        if wRatio > hRatio:
+            ratio = wRatio
+        else:
+            ratio = hRatio
+        return cv2.resize(img, (int(ratio * imgSize[1]), int(ratio * imgSize[0])))
+
 def addPadding(rect, padding, imgSize):
     rect['x'] -= padding
     rect['y'] -= padding
@@ -80,6 +107,10 @@ def getPageFromImage(img):
                 maxRect['w'] = w
                 maxRect['h'] = h
 
+    if maxRect['w'] <= 1 or maxRect['h'] <= 1:
+        print 'Warning: No Page Found'
+        return img
+
     contoursInPage = 0
     for coordinate in coordinates:
         x = coordinate[0]
@@ -102,6 +133,7 @@ def getSignatureFromPage(img):
     threshold, _ = cv2.threshold(src = gImg, thresh = 0, maxval = 255, type = cv2.THRESH_BINARY | cv2.THRESH_OTSU)
     cannyImg = cv2.Canny(image = gImg, threshold1 = 0.5 * threshold, threshold2 = threshold)
 
+    # The kernel is wide as most signature are wide
     kernel = cv2.getStructuringElement(shape = cv2.MORPH_RECT, ksize = (30, 1))
     cannyImg = cv2.morphologyEx(src = cannyImg, op = cv2.MORPH_CLOSE, kernel = kernel)
 
@@ -132,6 +164,10 @@ def getSignatureFromPage(img):
             maxRect['w'] = w
             maxRect['h'] = h
 
+    if maxRect['w'] <= 1 or maxRect['h'] <= 1:
+        print 'Warning: No Signature Found'
+        return img
+
     # Add padding so the signature is more visible
     maxRect = addPadding(rect = maxRect, padding = 10, imgSize = imgSize)
 
@@ -142,7 +178,7 @@ def getSignature(img):
 
     gImg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    # Adaptive Thresholding required the blocksize to be even and bigger than 1
+    # Adaptive Thresholding requires the blocksize to be even and bigger than 1
     blockSize = 1 / 8 * imgSize[0] / 2 * 2 + 1
     if blockSize <= 1:
         blockSize = imgSize[0] / 2 * 2 + 1
@@ -166,6 +202,9 @@ elif selection == 1:
 else:
     quit()
 
+# Make the image smaller if the image is too big as the signature extraction does not work properly with large images
+img = scaleImageDown(img)
+
 # Extract Signature
 page = getPageFromImage(img = img)
 signatureBlock = getSignatureFromPage(img = page)
@@ -178,8 +217,7 @@ buttons = ['Display on Screen', 'Export to File']
 selection = easygui.indexbox(msg = message, title = title, choices = buttons)
 
 if selection == 0:
-    cv2.imshow('Signature', signature)
-    cv2.waitKey(0)
+    displayImageToScreen(signature, mask)
 elif selection == 1:
     writeImageToFile(signature, mask)
 else:
